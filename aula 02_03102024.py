4d8672b1da30f5309480f0c78509f1be5647a4e3
CristinaMflores <cristinamendflores@gmail.com>
2024-10-04 21:39:29 -0300

    Add files via upload

---
+++ aula 02_03102024.py
@@ -0,0 +1,27 @@
+print("") #string
+'dentro dos tipos'
+'''
+'''
+
+temperatura=36
+
+if temperatura<37.5:
+    print("Voce nao esta com febre")
+elif temperatura==37.5:
+    print("Voce está febril.")  
+else:
+    print("Voce esta com febre.")         
+
+
+
+idade1=16
+idade2=18
+idade3=26
+
+if idade1<=16 and idade1<=18:
+    print("Nao entra na festa.")
+elif idade1>=18:
+    print(" entra na festa.")  
+else:
+    print(" esta na festa.") 
+
