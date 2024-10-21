273c43db0f0dd82bf72884d9762c7f781e5e4966
CristinaMflores <cristinamendflores@gmail.com>
2024-10-21 18:41:58 -0300

    Create aula 0917102024.py

---
+++ aula 0917102024.py
@@ -0,0 +1,3 @@
+#Leia três pares de números inteiros fornecidos pelo usuário, calcule e imprima a soma de cada par separadamente. Utilize 
+#tratamento de erros para garantir que os valores inseridos sejam válidos e, se o número for ÍMPAR, ter uma exceção personalizada.
+#####
