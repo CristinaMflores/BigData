20999c34a8fae0f1c96de170cd457aecf2c3c3c6
CristinaMflores <cristinamendflores@gmail.com>
2024-10-21 19:12:09 -0300

    Update aula 07_15102024.py

--- aula 07_15102024.py
+++ aula 07_15102024.py
@@ -2,4 +2,20 @@
 # uma sequência de 20 números pares e 20 números ímpares.
 # Obs: utilize estruturas de repetição para fechar esse conjunto
 # de números.
--
+
+numeros_pares = []
+numeros_impares = []
+
+while len(numeros_pares) < 20 or len(numeros_impares) < 20:
+     numero = int(input("Digite um número: "))
+  if len(numeros_pares) < 20:
+            numeros_pares.append(numero)
+            print(f"Número par adicionado: {numero}")
+        else:
+            print("Já foram adicionados 20 números pares.")
+    else:
+        if len(numeros_impares) < 20:
+            numeros_impares.append(numero)
+            print(f"Número ímpar adicionado: {numero}")
+        else:
+            print("Já foram adicionados 20 números ímpares.")
