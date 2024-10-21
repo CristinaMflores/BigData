9e616195d16a5778e1be17f5a2cb4e8a389668d1
CristinaMflores <cristinamendflores@gmail.com>
2024-10-21 19:07:47 -0300

    Update aula 09_17102024.py

--- aula 09_17102024.py
+++ aula 09_17102024.py
@@ -1,3 +1,12 @@
 #Leia três pares de números inteiros fornecidos pelo usuário, calcule e imprima a soma de cada par separadamente. Utilize 
 #tratamento de erros para garantir que os valores inseridos sejam válidos e, se o número for ÍMPAR, ter uma exceção personalizada.
-#####
+
+numeros_pares = []
+numeros_impares = []
+
+while len(numeros_pares) < 20 or len(numeros_impares) < 20:
+  
+  numero = int(input("Digite um número: "))
+  if len(numeros_pares) < 20:
+
+   
